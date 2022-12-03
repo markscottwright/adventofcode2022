53e129d87beafb7ca71d9024d895522de9ae296b
Mark Wright <markscottwrigth@gmail.com>
2022-12-03 08:01:30 -0500

    Add AllDays

---
+++ src/main/java/adventofcode2022/AllDays.java
@@ -0,0 +1,18 @@
+package adventofcode2022;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+public class AllDays {
+    public static void main(String[] args) {
+        List<Consumer<String[]>> days = List.of(Day1::main, Day2::main, Day3::main);
+        days.forEach(m -> {
+            StopWatch stopWatch = StopWatch.createStarted();
+            m.accept(args);
+            System.out.println(stopWatch.formatTime());
+            System.out.println();
+        });
+    }
+}
